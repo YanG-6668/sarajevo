cfd863b47cd7a4baf32f914455a32f3e3dac0f5c
YanG-6668 <goch.yannn@gmail.com>
2020-03-19 16:03:50 +0200

    added life

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
+const sections = document.querySelectorAll('.section');
+const viewHeight = window.innerHeight;
+
+const revealSection = () => {
+    sections.forEach(section => {
+        const distanceFromTop = section.getBoundingClientRect().top;
+        distanceFromTop < viewHeight - 0 ?
+            section.classList.add('scroll-reveal') :
+            section.classList.remove('scroll-reveal');
+    });
+};
+
+window.addEventListener('load', () => revealSection());
+window.addEventListener('scroll', () => revealSection());
+
 $(function() {
 
     $('.menu__btn').on('click', function() {
